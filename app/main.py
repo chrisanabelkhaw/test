2cfd07987f0fca5fcd9335cfc946caf6ffb0ec43
chrisanabelkhaw <anabel.khaw1408@gmail.com>
2021-10-31 14:37:24 +0800

    test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,41 @@
-from flask import Flask
- 
+import pickle
+import numpy as np
+import pandas as pd
+from flask import Flask, request, Response, render_template, jsonify
+
+
+#load model here 
 app = Flask(__name__)
- 
-@app.route("/")
-def home_view():
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+model = pickle.load(open('app/model.pkl', 'rb'))
+vectorizer = pickle.load(open('app/victorizer.pkl', 'rb'))
+
+@app.route('/') 
+def home(): 
+    return render_template('home.html') 
+
+@app.route('/predict', methods=['POST']) 
+def predict(): 
+    '''
+    For rendering results on HTML GUI
+    '''
+    if request.method =='POST':
+        message = request.form['message']
+        data=[message]
+        vect = vectorizer.transform(data).toarray()
+        my_prediction = model.predict(vect)
+        category = "NA"
+        if my_prediction == '1':
+            category = "Class A"
+        elif my_prediction == '2':
+            category = "Class B"
+        elif my_prediction == '3':
+            category = "Class C"
+        elif my_prediction == '4':
+            category = "Class D"
+        elif my_prediction == '5':
+            category = "TBA"
+        elif my_prediction == '6':
+            category = "NA"
+
+    
+    return render_template('result.html', prediction = category) 
